209d8ee1c00d086c9563dc950afd1eb7c7493674
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-09-21 14:39:24 +0300

    C01/ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	std::string		string("HI THIS IS BRAIN");
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "Address by string = " << &string << std::endl;
+	std::cout << "Address by stringPTR = " << stringPTR << std::endl;
+	std::cout << "Address by stringREF = " << &stringREF << std::endl;
+	std::cout << "Content by stringPTR = " << *stringPTR << std::endl;
+	std::cout << "Content by stringREF = " << stringREF << std::endl;
+	
+	return (0);
+}
