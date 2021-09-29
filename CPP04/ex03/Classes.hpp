108c7a27ee9b084f2e7de69b76d303e705ca9afa
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-09-29 19:55:08 +0300

    ex3

---
+++ CPP04/ex03/Classes.hpp
@@ -0,0 +1,33 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <iostream>
+#include "Amateria.hpp"
+
+class	Character
+{
+private:
+	std::string	_name;
+public:
+	Character(/* args */);
+	~Character();
+};
+
+class	Ice
+{
+private:
+	std::string	_name;
+public:
+	Ice(/* args */);
+	~Ice();
+};
+
+class	Cure
+{
+private:
+	std::string	_name;
+public:
+	Cure(/* args */);
+	~Cure();
+};
+#endif
