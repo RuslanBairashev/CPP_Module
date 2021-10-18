233578e240b02fb41236ea7e173c7b6e44ec30c0
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-10-18 20:36:10 +0300

    C07 done

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -12,15 +12,15 @@ private:
 	T 			*_array;
 	unsigned	_length;
 public:
-	Array() { _array = new T[0]; _length = 0;/* T[0] = NULL;*/ }
-	Array(unsigned int n)
-	{
-		//assert(n > 0);
+	Array(): _array(new T[0]), _length(0) {}
+	Array(unsigned int n): _array(new T[n]), _length(n) {}
+	/*{
+		assert(n >= 0);
 		_length = n;
 		_array = new T[n];
-		//for(unsigned int = 0; i < n; i++)
-		//	_array[i] = T;
-	}
+		for(unsigned int i = 0; i < n; i++)
+			_array[i] = 0;
+	}*/
 	~Array() { delete[] _array; }
 
 	Array(const Array& c_name)
@@ -33,20 +33,30 @@ public:
 	{
 		if (this == &c_name)
 			return *this;
+		delete [] _array;
+		_length = c_name._length;
 		_array = new T[c_name._length];
 		*_array = *c_name._array;
 		return *this;
 	}
 
-	int&	operator[](unsigned int ind)
+	T&	operator[](unsigned int ind)
 	{
-		//assert(ind >= 0 && ind < _length);
+		if(ind < 0 || ind > (_length - 1))
+			throw OutOfRange();
 		return _array[ind];
 	}
 
 	unsigned int	size() const { return _length; }
+
+	class OutOfRange: public std::exception
+	{
+		public:
+			const char* what() const throw()
+			{
+				return "Array::OutOfRangeException";
+			}
+	};
 };
-
-
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "Array.hpp"
 
+class Tclass
+{
+private:
+	int	_value;
+public:
+	int	getValue() { return _value; }
+};
+
 #define MAX_VAL 750
+
 int main()
 {
     Array<int> numbers(MAX_VAL);
@@ -27,29 +36,40 @@ int main()
             return 1;
         }
     }
-    /*try
+    try
     {
         numbers[-2] = 0;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
-    }*/
-    /*try
+    }
+    try
     {
         numbers[MAX_VAL] = 0;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
-    }*/
+    }
 
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
     }
+	std::cout << "numbers size func test: " << numbers.size() << std::endl;
     delete [] mirror;//
+
+	Array<float> empty;
+	std::cout << "empty size func test: " << empty.size() << std::endl;
+	std::cout << "empty index access test: " << empty[0] << std::endl;
+	Array<int> test(15);
+	std::cout << "test size func test: " << test.size() << std::endl;
+	std::cout << "test index access test: " << test[10] << std::endl;
+	test[10] = 500;
+	std::cout << "test index access test: " << test[10] << std::endl;
+	Array<Tclass> test_cl(8);
+	std::cout << "test_cl size func test: " << test_cl.size() << std::endl;
+
     return 0;
 }
-
-//https://metanit.com/cpp/c/5.11.php
