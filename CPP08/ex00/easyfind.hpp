6b0a5e2ac1a2e93ee4d59e5fbfb2a91ed80fdaa0
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-10-19 20:46:10 +0300

    C08 begin

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -2,13 +2,25 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <deque>
-
+#include <list>
 
 template<typename T>
 void	easyfind(T cont, int n)
 {
+	typename T::const_iterator	it;
+	/*for (it = cont.begin(); it != cont.end(); ++it)
+	{
+		if (*it == n)
+			std::cout << "found one\n";
+	}*/
+	it = std::find(cont.begin(), cont.end(), n);
+	if (it != cont.end())
+		std::cout << "found one: " << &it << std::endl;
+	else 
+		std::cout << "Error: not found\n";
 	
 }
 
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -5,4 +5,18 @@ int	main()
 	std::vector<int>	vect;
 	std::deque<int>		deq;
 
+	for(int count = 0; count < 5; ++count)
+	{
+		vect.push_back(1 + count);
+		deq.push_back(1 * count);
+	}
+
+	for(int count = 0; count < 5; ++count)
+	{
+		std::cout << "vect: " << vect[count] << ' ' << "deq: " << deq[count] << std::endl;
+	}
+
+	easyfind(vect, 10);
+	easyfind(deq, 2);
+	return 0;
 }
